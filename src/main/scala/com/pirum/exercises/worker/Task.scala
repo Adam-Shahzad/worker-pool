5a6a964fe3d4c84b241b2c1517ba4ba4181e6b81
Adam-Shahzad <Adam.shahzad@outlook.com>
2021-05-12 11:10:53 +0100

    Update Task.scala

--- src/main/scala/com/pirum/exercises/worker/Task.scala
+++ src/main/scala/com/pirum/exercises/worker/Task.scala
@@ -1,3 +1,10 @@
 package com.pirum.exercises.worker
+import scala.concurrent.duration.Duration
 
 trait Task
+case class SuccessfulTask(seconds: Duration) extends Task
+case class FailedTask(seconds: Duration) extends Task
+case class TimeoutTask() extends Task
+
+
+case class Result(successful: Seq[SuccessfulTask], failed: Seq[FailedTask], timedOut: Seq[TimeoutTask])
