1427c7ca498d6efd8dd3073a69c144b1a6685ecd
Adam-Shahzad <Adam.shahzad@outlook.com>
2021-05-12 11:11:17 +0100

    Update MainSpec.scala

--- src/test/scala/com/pirum/exercises/worker/MainSpec.scala
+++ src/test/scala/com/pirum/exercises/worker/MainSpec.scala
@@ -1,5 +1,59 @@
 package com.pirum.exercises.worker
+import org.scalatest.concurrent.Eventually
+import org.scalatest.concurrent.ScalaFutures.convertScalaFuture
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
+import org.scalatest.wordspec.AnyWordSpec
 
-object MainSpec {
-  
+object MainSpec extends AnyWordSpec with Matchers with Eventually {
+  "Main.program" should {
+    "Handle Failed and successful tasks" in {
+      eventually {
+
+      Main
+        .program(
+          Seq(
+            FailedTask(3.seconds),
+            SuccessfulTask(4.seconds),
+            SuccessfulTask(2.seconds),
+            FailedTask(1.seconds)
+          ),
+          timeout = 8.seconds,
+          workers = 4
+        )
+        .futureValue match {
+        case res => res shouldBe Result(
+          Seq(SuccessfulTask(4.seconds), SuccessfulTask(2.seconds)),
+          Seq(FailedTask(3.seconds), FailedTask(1.seconds)),
+          Seq.empty)
+        case _ => fail(s"something went wrong")
+      }
+    }
+    }
+
+    "Handle Failed and successful and timeout tasks" in {
+      eventually {
+
+        Main
+          .program(
+            Seq(
+              FailedTask(3.seconds),
+              TimeoutTask(),
+              SuccessfulTask(4.seconds),
+              SuccessfulTask(2.seconds),
+              FailedTask(1.seconds)
+            ),
+            timeout = 8.seconds,
+            workers = 4
+          )
+          .futureValue match {
+          case res => res shouldBe Result(
+            Seq(SuccessfulTask(4.seconds), SuccessfulTask(2.seconds)),
+            Seq(FailedTask(3.seconds), FailedTask(1.seconds)),
+            Seq(TimeoutTask()))
+          case _ => fail(s"something went wrong")
+        }
+      }
+    }
+  }
 }
